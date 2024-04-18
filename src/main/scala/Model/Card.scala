6e2c6ced75e89844911ca4bf8d8e1980742815df
arutepsu <cgmartchon@gmail.com>
2024-04-18 19:55:43 +0200

    soccer.txt before merge conflict

---
+++ src/main/scala/Model/Card.scala
@@ -0,0 +1,24 @@
+package Model
+
+import Model.CardObject._
+import Model.Suit.Suit
+
+case class Card(value: CardValue, suit: Suit) {
+  override def toString: String = s"${valueToString(value)} of ${Suit.suitToString(suit)}"
+
+  private def valueToString(value: CardValue): String = value match {
+    case Ace => "Ace"
+    case Two => "2"
+    case Three => "3"
+    case Four => "4"
+    case Five => "5"
+    case Six => "6"
+    case Seven => "7"
+    case Eight => "8"
+    case Nine => "9"
+    case Ten => "10"
+    case Jack => "Jack"
+    case Queen => "Queen"
+    case King => "King"
+  }
+}
